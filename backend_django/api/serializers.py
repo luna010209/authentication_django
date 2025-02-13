66f97b7d4e9d02b461f61098b6c7915f796a3afc
Lunado <liendhhha140217@gmail.com>
2025-02-13 23:37:47 +0900

    Update backend

--- backend_django/api/serializers.py
+++ backend_django/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Note
 class UserSerializer(serializers.ModelSerializer):
   password = serializers.CharField(write_only=True, style={"input_type": "password"})
   class Meta:
@@ -11,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
   def create(self, data):
     user = User.objects.create_user(**data)
     return user
+  
+class NoteSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Note
+    fields = ["id", "title", "content", "created_at", "author"]
+    extra_kwargs = {"author": {"read_only":True}}
